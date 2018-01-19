5ab993e92027583acf8a1a04f18df6a924f01dec
bieito98 <bieitobf9@gmail.com>
2018-01-19 18:53:20 +0100

    Added sounds

--- game/scripts.js
+++ game/scripts.js
@@ -1,17 +1,34 @@
 var screenX = window.innerWidth;
 var screenY = window.innerHeight;
 
+function playShoot (aud) {
+	var sound = document.createElement("audio");
+	var sound_source;
+
+	switch (aud) {
+		case 0: sound_source = "miss.mp3";
+			break;
+		case 1: sound_source = "shoot.mp3";
+	}
+
+	sound.setAttribute("src", sound_source);
+	sound.play();
+
+	//sound.remove();
+}
+
 function restartGame () {
 	document.getElementById("score").innerHTML = "0:0";
-	document.getElementById("html").setAttribute("onclick", "refreshScore(1,0);");;
+	document.getElementById("html").setAttribute("onclick", "refreshScore(1,0)");;
 	startGame(10);
 }
 
 function endGame () {
-	var score = document.getElementById("score").innerHTML.split(":");
+	var scorebox = document.getElementById("score");
+	score = scorebox.innerHTML.split(":");
 	var htmldoc = document.getElementById("html");
 
-	alert("Game ended. Your score:\n\t" + (score[1]/score[0] * 100).toFixed(0) + " points!");
+	scorebox.innerHTML = (score[1]/score[0] * 100).toFixed(0) + "pts";
 
 	htmldoc.setAttribute("onclick", "restartGame();");
 }
@@ -19,6 +36,7 @@ function endGame () {
 function refreshScore (miss, target) {
 	var score = document.getElementById("score").innerHTML.split(":");
 
+	playShoot( (miss == 1) ? (0) : (1) );
 	document.getElementById("score").innerHTML = (Number(score[0]) + miss)
 							+ ":" + (Number(score[1]) + target);
 	if (score[1] == 10) {
@@ -34,7 +52,6 @@ async function killedBox (id) {
 	var box = document.getElementById(id)
 
 	box.style.backgroundColor = "#a00";
-	refreshScore(0,1);
 	box.removeAttribute("onclick");
 	await sleep(100);
 	document.getElementById("body").removeChild(box);
@@ -48,7 +65,7 @@ function createBox (num_id) {
 	box.style.left = Math.floor(Math.random() * (screenX-64) ) + "px";
 	box.style.top = Math.floor(Math.random() * (screenY-64)) + "px";
 	box.id = "box_no" + num_id;
-	box.setAttribute("onclick", "killedBox('" + box.id + "');");
+	box.setAttribute("onclick", "refreshScore(0,1);killedBox('" + box.id + "');");
 
 	return box;
 }
